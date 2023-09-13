645c5f68241b0e0c84489f0d7c56c8d9a9bbc669
Sandesh Shrestha <sandeshstha078@gmail.com>
2023-09-13 15:11:07 +0545

    added signup screen

--- auth/lib/screens/signup_screen.dart
+++ auth/lib/screens/signup_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../utils/common_textfield.dart';
+import 'login_screen.dart';
+
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
 
@@ -8,8 +11,97 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController namecontrollor = TextEditingController();
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1F0F0),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Register',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: CommonTextField(
+                controller: namecontrollor,
+                labelText: "Name",
+                tohide: false,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: CommonTextField(
+                  controller: emailcontroller,
+                  labelText: "Email",
+                  tohide: false),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: CommonTextField(
+                controller: passwordcontroller,
+                labelText: "password",
+                tohide: true,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Already have an account?",
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
+                    );
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
