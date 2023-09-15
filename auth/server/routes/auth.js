26d91844334b7bb4cb4c8cf1d8be9d4b9506c27e
Sandesh Shrestha <sandeshstha078@gmail.com>
2023-09-15 16:29:33 +0545

    added signin route

--- auth/server/routes/auth.js
+++ auth/server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const authRoute = express.Router()
 const User = require('./../models/user.model')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 // signup route
 authRoute.get('/',(req, res)=>{res.send('hello')})
@@ -34,6 +35,35 @@ authRoute.post('/api/signup',async (req, res)=>{
     }
 })
 
+
+// sign in route
+
+authRoute.post('/api/sigin',async(req, res)=>{
+    try{
+        const {email , password} = req.body
+    
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({msg: 'User with this email does not exist'})
+        }
+    
+        const matchPassword = await bcrypt.compare(password , user.password)
+        if(!matchPassword){
+           return res.status(400).json({msg: "Invalid Password"})
+        }
+        
+        // creating token
+        const payload = {id : user._id}
+        const secretKey = 'mysecret@key'
+    
+        const token  = jwt.sign(payload, secretKey)
+        res.json({token , ...user._doc})
+    }
+    catch(e){
+        res.json({error:e.message})
+    }
+
+})
 
 
 module.exports = authRoute
